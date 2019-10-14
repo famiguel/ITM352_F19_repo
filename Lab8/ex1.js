0e7a4361b5a00cb50443a6a99316f41e96d77430
Faye Miguel <famiguel@hawaii.edu>
2019-10-13 23:41:41 -1000

    update

---
+++ Lab8/ex1.js
@@ -0,0 +1,7 @@
+var age = 21;
+var number = 1;
+while (number != age) {
+    console.log(`Are you ${number} years old?`);
+    number++
+}
+console.log(`You must be ${number} years old!`);
---
+++ Lab8/ex3.js
@@ -0,0 +1,6 @@
+var age = 21;
+ 
+for(var number = 50; number > age; number--){
+    console.log(`are you ${number} years old?`);
+}
+console.log(`You must be ${number} years old!`);
