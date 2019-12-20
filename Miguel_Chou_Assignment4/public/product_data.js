64b9b906a0285ead44772ef271a5990bc418eec5
Faye Miguel <famiguel@hawaii.edu>
2019-12-19 18:09:33 -1000

    update final

--- Miguel_Chou_Assignment4/public/product_data.js
+++ Miguel_Chou_Assignment4/public/product_data.js
@@ -1,31 +1,31 @@
-//This file containes the JSON object for the products available in the merchandise shop.
+//This file containes the JSON object for the products
 
 var products = 
 [
     {
-        "item": "White Jersey",
-        "price": 35.00,
-        "image": "AMA_jersey1.jpg"
-    },
-    {
-        "item": "Black Jersey",
-        "price": 35.00,
-        "image": "AMA_jersey2.png"
-    },
-    {
-        "item": "Stickers",
-        "price": 5.00,
-        "image": "AMA_sticker.png"
-    },
-    {
-        "item": "Graduation Stoles",
-        "price": 35.00,
-        "image": "AMA_stole.png"
-    },
-    {
-        "item": "Sweatshirt",
+        "bracelet": "Silver Turtle",
         "price": 25.00,
-        "image": "AMA_sweatshirt.png"
+        "image": "bracelet1.png"
+    },
+    {
+        "bracelet": "Silver Mermaid",
+        "price": 25.00,
+        "image": "bracelet2.png"
+    },
+    {
+        "bracelet": "Crystal Paw",
+        "price": 35.00,
+        "image": "bracelet3.png"
+    },
+    {
+        "bracelet": "Pair of Wings",
+        "price": 45.00,
+        "image": "bracelet4.png"
+    },
+    {
+        "bracelet": "Layered Rose Gold Turtle",
+        "price": 65.00,
+        "image": "bracelet5.jpg"
     },
 ]
 if(typeof module != 'undefined') {
--- Miguel_Chou_Assignment4/server.js
+++ Miguel_Chou_Assignment4/server.js
@@ -1,6 +1,5 @@
 //The following codes allows to do server side processing.
 //Resource: Lab13 Assignment
-//Mark Chou & Faye Miguel 
 
 const querystring = require('querystring');
 var express = require('express'); //code for server
@@ -15,117 +14,72 @@ var itemquantity = {};
 
 app.use(myParser.urlencoded({ extended: true }));
 
-//go to invoice if quantity values are good, if not, redirect back to order page 
-app.get("/process_page", function (request, response) {
-   //check for valid quantities
-   //look up request.query
-   itemquantity = request.query;
-   params = request.query;
-   console.log(params);
-   if (typeof params['purchase_submit'] != 'undefined') {
-      has_errors = false; // assume that quantity values are valid
-      total_qty = 0; // check if there are values in the first place, so see if total > 0
-      for (i = 0; i < products.length; i++) {
-         if (typeof params[`quantity${i}`] != 'undefined') {
-            a_qty = params[`quantity${i}`];
-            total_qty += a_qty;
-            if (!isNonNegInt(a_qty)) {
-               has_errors = true; // see if there is invalid data
-            }
-         }
-      }
-      qstr = querystring.stringify(request.query);
-      // redirect to invoice if quantity data is valid or respond to invalid data
-      if (has_errors || total_qty == 0) {
-         //redirect to products page if quantity data is invalid
-         qstr = querystring.stringify(request.query);
-         response.redirect("product_page.html?" + qstr);
-      } else { //the quantity data is okay for the invoice
-         response.redirect("login.html?" + qstr);
-      }
-   }
-});
-
-
-//if quantity data valid, send them to the invoice
-
-function isNonNegInt(q, returnErrors = false) {
-   errors = []; // assume that quantity data is valid 
-   if (q == "") { q = 0; }
-   if (Number(q) != q) errors.push('Not a number!'); //check if value is a number
-   if (q < 0) errors.push('Negative value!'); //check if value is a positive number
-   if (parseInt(q) != q) errors.push('Not an integer!'); //check if value is a whole number
-   return returnErrors ? errors : (errors.length == 0);
-}
-
-fs = require('fs'); //uses file system module
-
-//only open file if it exists
-if (fs.existsSync(filename)) {
-   stats = fs.statSync(filename) //gets stats from file
-
-   data = fs.readFileSync(filename,'UTF-8');
-   console.log(typeof data);
-   users_reg_data = JSON.parse(data);
-}
-
-
 //go to login page
-app.get("/login.html", function (request, response) {
+app.get("/index.html", function (request, response) {
    str = `
+   <!DOCTYPE html>
    <html lang="en">
-
-   <head>
-       <meta charset="UTF-8">
-       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-       <title>Marikit Login Page</title>
-   
-       <link href="product_style.css" rel="stylesheet">
-   </head>
-   
+   <title>American Marketing Association</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+   <style>
+   body,h1,h2,h3,h4,h5 {font-family: "Poppins", sans-serif}
+   body {font-size:16px;}
+   .w3-half img{margin-bottom:-6px;margin-top:16px;opacity:0.8;cursor:pointer}
+   .w3-half img:hover{opacity:1}
+   </style>
    <body>
-       <!--the following codes is for the navigation bar-->
-       <ul>
-               <li><a href="./index.html">Home</a></li>
-               <li><a href="./product_page.html">Collection</a></li>
-               <li><a class="active" href="./login.html">Log In</a></li>
-       </ul>
    
-       <!--this code puts the whole body of the website not overlapping the navbar-->
-       <div style="margin-left:25%;padding:1px 16px;height:1000px;">
+   <!-- Top menu on small screens -->
+   <header class="w3-container w3-top w3-hide-large w3-blue w3-xlarge w3-padding">
+     <a href="javascript:void(0)" class="w3-button w3-blue w3-margin-right" onclick="w3_open()">☰</a>
+     <span>Company Name</span>
+   </header>
    
-           <h1>Marikit</h1>
-           <h2> Log in to Continue to your Purchase!</h2>
-           <form name="loginform" method="POST">
-               <div>
-                   <input type="text" name="username" size="40" placeholder="enter username"><br />
-                   <input type="password" name="password" size="40" placeholder="enter password"><br />
-                   <input type="submit" value="login" id="submit"> </div>
-           </form>
-   </body>
+   <!-- Overlay effect when opening sidebar on small screens -->
+   <div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
    
-   <h2>No Account? Join Us Below!</h2>
+   <!-- !PAGE CONTENT! -->
+   <div class="w3-main" style="margin-left:340px;margin-right:40px">
    
-   <body>
+     <!-- Header -->
+     <div class="w3-container" style="margin-top:80px" id="showcase">
+       <h1 class="w3-jumbo"><b>American Marketing Association</b></h1>
+       <h1 class="w3-xxxlarge w3-text-blue"><b>Login Page</b></h1>
+       <h3 class="w3-xlarge w3-text-blue"><b>Access our weekly newsletter.</b></h3>
+       <hr style="width:50px;border:5px solid blue" class="w3-round">
+     </div>
+   
+     <form name="loginform" method="POST">
        <div>
-           <form action="./registration.html">
-           <input type="submit" class="button" value="Create Account Here" id="regpage" name="register_here">
-           </form>
-       </div>
-       
-       </body>
-       
-       </html>
-       
+           <input type="text" name="username" size="40" placeholder="enter username"><br/>
+   <br>
+           <input type="password" name="password" size="40" placeholder="enter password"><br/>
+   <br>
+           <input type="submit" value="login" id="submit"> </div>
+   </form>
+   </body>
+     
+    
+   <!-- End page content -->
+   </div>
+   
+   <!-- W3.CSS Container -->
+   <div class="w3-container w3-padding-32" style="margin-top:75px;padding-right:58px"><p class="w3-right"><a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity"></a></p></div>
+   
+   </body>
+   </html>
+          
 `;
    response.send(str);
 
 });
 
-app.post("/login.html", function (request, response) {
+app.post("/index.html", function (request, response) {
    // Process login form POST and redirect to logged in page if ok, back to login page if not
-   console.log(itemquantity);
+
    the_username = request.body.username;
    console.log(the_username, "Username is", typeof (users_reg_data[the_username]));
    //validate login data
@@ -133,17 +87,17 @@ app.post("/login.html", function (request, response) {
       //To check if the username exists in the json data
       if (users_reg_data[the_username].password == request.body.password) {
          //make the query string of prod quant needed for invoice
-         theQuantQuerystring = qs.stringify(itemquantity);
-         response.redirect('/cart_page.html?' + theQuantQuerystring + `&username=${the_username}`);
-       
+
+         response.redirect('/welcome.html?' + `&username=${the_username}`);
+         //ADDS USERNAME INFO TO INVOICE
       } else {
-         response.redirect('./login.html?')
-         
+         response.redirect('./index.html?')
+         //IN ASSIGNMENT, SHOW THERE IS AN ERROR
       }
    }
 });
 
-app.get("/registration.html", function (request, response) {
+app.get("/index.html", function (request, response) {
    // Give a simple register form
 
    str = `
@@ -157,12 +111,6 @@ app.get("/registration.html", function (request, response) {
        <script>src ="server.js"</script>
    </head>
    <body>
-   <!--the following codes is for the navigation bar-->
-   <ul>
-       <li><a href="./index.html">Home</a></li>
-       <li><a href="./product_page.html">Collection</a></li>
-       <li><a href="./login.html">Log In</a></li>
-   </ul>
 
    <!--this code puts the whole body of the website not overlapping the navbar-->
    <div style="margin-left:25%;padding:1px 16px;height:1000px;">
@@ -182,7 +130,7 @@ app.get("/registration.html", function (request, response) {
    response.send(str);
 });
 
-app.post("/registration.html", function (request, response) {
+app.post("/index.html", function (request, response) {
    // process a simple register form
    console.log(itemquantity);
    the_username = request.body.username;
@@ -204,10 +152,10 @@ app.post("/registration.html", function (request, response) {
       users_reg_data[username].password = request.body.password;
       users_reg_data[username].email = request.body.email;
 
-      theQuantQuerystring = qs.stringify(braceletquantity);
+      theQuantQuerystring = qs.stringify(itemquantity);
       fs.writeFileSync(filename, JSON.stringify(users_reg_data));
       response.redirect("/cart_page.html?" + theQuantQuerystring + `&username=${the_username}`);
-      
+
 
    }
 });
